50bee2a452a64c6ca2dbbf535ee411fc110d7d8c
karunendumishra <68456371+karunendumishra@users.noreply.github.com>
2022-05-23 13:07:20 +0530

    commit

--- libs/remix-ui/vertical-icons-panel/src/lib/components/BasicLogo.tsx
+++ libs/remix-ui/vertical-icons-panel/src/lib/components/BasicLogo.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function BasicLogo () {
-  return (<img src='assets/img/xide_logo.svg' height={"40px"}/>
+  return (<img src='assets/img/xide_logo.png' width={"40px"}/>
   )
 }
 
